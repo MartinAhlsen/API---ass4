0fb5055be5e889a287a22f3ec6affc9d2c04d500
MartinAhlsen <martin.ahlsen@gmail.com>
2024-02-12 11:39:50 +0100

    add html from api call

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,28 @@
 $(() => {
   console.log("hej");
+  const END_POINT = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+  const getArtist = async (url) => {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+
+    let drink = data.drinks[0];
+
+    console.log(drink.strDrink);
+
+    $(".cocktail-wrapper")
+      .empty()
+      .append(
+        `
+        <h2>${drink.strDrink}</h2>
+        <p class="instructions">${drink.strInstructions}</p>
+        <img src="${drink.strDrinkThumb}">
+      `
+      );
+  };
+
+  $(".button-random").on("click", () => {
+    getArtist(END_POINT);
+  });
 });
